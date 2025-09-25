d9795b8ee752cd213302c6f727aa06a9ab8b231e
Konosuke Sakai <konosukeam@gmail.com>
2025-09-25 10:26:11 +0900

    feat(App.tsx): wrap Canvas component in a div to ensure full-screen rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 
 function App() {
-	return <Canvas />;
+	return (
+		<div className="h-screen w-screen">
+			<Canvas />
+		</div>
+	);
 }
 
 export default App;
