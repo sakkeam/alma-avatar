0815a6795e72712a0f7525fed3f86f4bd62ec043
Konosuke Sakai <konosukeam@gmail.com>
2025-09-25 14:36:29 +0900

    feat(App.tsx): integrate VR support by adding XR store and Enter VR button to enhance user experience in virtual reality

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
-import { CameraControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { createXRStore, XR } from "@react-three/xr";
 import AvatarModel from "./AvatarModel";
+
+const store = createXRStore();
 
 function App() {
 	return (
 		<div className="h-screen w-screen">
+			<button type="button" onClick={() => store.enterVR()}>
+				Enter VR
+			</button>
 			<Canvas shadows>
-				<color attach="background" args={["#6aa2f5"]} />
-				<fogExp2 attach="fog" args={["#6aa2f5", 0.025]} />
-				<ambientLight color="#ffffff" intensity={0.5} />
-				<directionalLight castShadow position={[-5, 5, -5]} intensity={1.5} />
+				<XR store={store}>
+					<color attach="background" args={["#6aa2f5"]} />
+					<fogExp2 attach="fog" args={["#6aa2f5", 0.025]} />
+					<ambientLight color="#ffffff" intensity={0.5} />
+					<directionalLight castShadow position={[-5, 5, -5]} intensity={1.5} />
 
-				<PerspectiveCamera makeDefault position={[0, 1, 5]} />
-				<CameraControls />
-
-				<mesh
-					position={[0, 0, 0]}
-					receiveShadow
-					rotation={[-Math.PI / 2, 0, 0]}
-				>
-					<planeGeometry args={[200, 200]} />
-					<meshStandardMaterial color="#ffffff" opacity={0.5} transparent />
-				</mesh>
-				<AvatarModel />
+					<mesh
+						position={[0, 0, 0]}
+						receiveShadow
+						rotation={[-Math.PI / 2, 0, 0]}
+					>
+						<planeGeometry args={[200, 200]} />
+						<meshStandardMaterial color="#ffffff" opacity={0.5} transparent />
+					</mesh>
+					<AvatarModel />
+				</XR>
 			</Canvas>
 		</div>
 	);
